c3c0c0da806be625a503b8480c75e678397046d9
Prajwal R <prajwal02024@gmail.com>
2025-09-04 23:54:27 +0530

    Create start-server.bat

---
+++ server/start-server.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Starting TSM Travells Node.js Server...
+echo.
+echo Make sure you have:
+echo 1. Node.js installed
+echo 2. Updated config.js with your Gmail app password
+echo 3. Gmail 2FA enabled
+echo.
+echo Starting server on port 3000...
+echo.
+npm start
+pause
