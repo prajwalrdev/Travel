d2f8faab982766e641e4d9fe39d94fa7c68faeaa
Prajwal R <prajwal02024@gmail.com>
2025-09-04 23:54:55 +0530

    Create config.js

---
+++ server/config.js
@@ -0,0 +1,19 @@
+// Email Configuration for Nodemailer
+module.exports = {
+  email: {
+    user: 'mangaloretaxicabservices@gmail.com',
+    pass: 'pnkdxplvgfxcmeyy', // You need to set this in your environment
+    adminEmail: 'mangaloretaxicabservices@gmail.com'
+  },
+  smtp: {
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    service: 'gmail'
+  },
+  company: {
+    name: 'TSM Travells',
+    phone: '+91 8861505754',
+    address: 'Ground Floor, GHS Opposite Tara clinic, Hampankatta Mangalore 575001'
+  }
+};
